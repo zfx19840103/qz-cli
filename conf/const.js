0e5abbd09d59175bc1226cc569c113ba6bf60124
zfx15652559172 <zfx15652559172@126.com>
2022-02-17 10:21:36 +0800

    init

--- conf/const.js
+++ conf/const.js
@@ -1,7 +1,7 @@
 module.exports = {
-  CLI_NAME: 'allen-cli',
-  NODE_ENV: process.env.NODE_ENV,
-  CUSTOMIZE_PROMPT: "customize_prompt.js",
-  TEMPLATE_NAME: './download-temp',
-  FILE_IGNORE: '.fileignore' // 定义忽略清单的文件名
+    CLI_NAME: 'qz-cli',
+    NODE_ENV: process.env.NODE_ENV,
+    CUSTOMIZE_PROMPT: "customize_prompt.js",
+    TEMPLATE_NAME: './download-temp',
+    FILE_IGNORE: '.fileignore' // 定义忽略清单的文件名
 }
